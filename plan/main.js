cb95e4b37b7af18f0fcc58e01597d46bd84d81a4
REBEN <abdirabbani59@gmail.com>
2024-05-30 09:05:48 +0700

    update error

--- plan/main.js
+++ plan/main.js
@@ -1,14 +1,15 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {
+    OrbitControls
+} from 'three/addons/controls/OrbitControls.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({
     antialias: true
-})
+});
 
 renderer.setSize(w, h);
-
 document.body.appendChild(renderer.domElement);
 
 const pov = 45;
@@ -17,31 +18,58 @@ const near = 0.1;
 const far = 1000;
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(pov, aspect, near, far);
+const orbit = new OrbitControls(camera, renderer.domElement);
 
-const orbit = new OrbitControls(camera, renderer.domElement)
-
-camera.position.set(10, 15, -22)
-
+camera.position.set(10, 15, -22);
 orbit.update();
 
-const geo = new THREE.PlaneGeometry(20, 20);
+const planeMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+        visible: false
+    })
+);
+planeMesh.rotateX(-Math.PI / 2);
+scene.add(planeMesh);
+planeMesh.name = 'ground';
 
-const mat = new THREE.MeshStandardMaterial({
-    side: THREE.DoubleSide
-})
+const highlightMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide
+    })
+);
 
-const planMesh = new THREE.Mesh(geo, mat);
-planMesh.rotateX(-Math.PI / 2)
-scene.add(planMesh);
+highlightMesh.rotateX(-Math.PI / 2);
+highlightMesh.position.set(0.5, 0, 0.5);
+scene.add(highlightMesh);
 
-const grid = new THREE.GridHelper(20,20)
+const mousePosition = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+let intersects;
+
+window.addEventListener('mousemove', function (e) {
+    console.log(e);
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = - (e.clientY / window.innerHeight) * 2 + 1; // Fix this line
+    raycaster.setFromCamera(mousePosition, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    intersects.forEach(function (intersect) {
+        if (intersect.object.name === 'ground') {
+            const highlightPos = new THREE.Vector3().copy(intersect.point).floor().addScalar(0.5);
+            highlightMesh.position.set(highlightPos.x, 0, highlightPos.z);
+        }
+    });
+});
+
+const grid = new THREE.GridHelper(20, 20);
 scene.add(grid);
 
 function animation() {
     requestAnimationFrame(animation);
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animation())
+renderer.setAnimationLoop(animation); // Fix this line
